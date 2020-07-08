f2b6a7997aa0eac81e93ad3bfbfea1d5bd037db7
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-08 16:35:31 +0900

    [add]画像投稿機能を追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2faeec22d4cb0fe213c9ecfb492165bf66f6707184f8e1138c4074db8a4280c410c6ac086769fbd6c7772f89e513f708ac2e286a831e7a8609423e03e5580013'
